72fd77bcae2360d3c1ff3ab9ca9da009e638c48d
Kevin Broh-Kahn <bkmobiledevelopment@gmail.com>
2015-01-08 15:52:38 -0600

    Move load task from library to individual app. Make preview and report links app-specific

--- app/src/main/java/org/boardgamers/wbc/TournamentFragment.java
+++ app/src/main/java/org/boardgamers/wbc/TournamentFragment.java
@@ -8,4 +8,14 @@ public class TournamentFragment extends CL_TournamentFragment {
   protected Class getEventActivityClass() {
     return EventActivity.class;
   }
+
+  @Override
+  protected String getPreviewLink(String tournament) {
+	return "http://boardgamers.org/yearbkex/" + tournament + "pge.htm";
+  }
+
+  @Override
+  protected String getReportLink(String tournament) {
+	return "http://boardgamers.org/yearbook13/" + tournament + "pge.htm";
+  }
 }
